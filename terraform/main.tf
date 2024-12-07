19287c3122db65479690a19fe5171d4a1d845a2c
Valerio Tuveri <valerio.tuveri@vrmedia.it>
2024-12-07 17:30:02 +0100

    add scrips install wordpress

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,36 +29,7 @@ resource "aws_instance" "wordpress" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.wordpress_sg.id]
 
-  user_data = <<-EOF
-              #!/bin/bash
-              # Install Nginx
-              amazon-linux-extras install nginx1 -y
-              systemctl start nginx
-              systemctl enable nginx
-
-              # Install PHP and required extensions
-              amazon-linux-extras install php8.0 -y
-              yum install -y php-mysqlnd php-fpm
-
-              # Download and install WordPress
-              WORDPRESS_VERSION="${var.wordpress_version}"
-              if [ "$WORDPRESS_VERSION" = "latest" ]; then
-                wget https://wordpress.org/latest.tar.gz
-              else
-                wget https://wordpress.org/wordpress-$WORDPRESS_VERSION.tar.gz
-              fi
-              tar -xzf wordpress-${WORDPRESS_VERSION}.tar.gz
-              cp -r wordpress/* /usr/share/nginx/html/
-              chown -R nginx:nginx /usr/share/nginx/html/
-              chmod -R 755 /usr/share/nginx/html/
-
-              # Configure PHP
-              sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/' /etc/php.ini
-
-              # Start PHP-FPM
-              systemctl start php-fpm
-              systemctl enable php-fpm
-              EOF
+  user_data = file("${path.module}/../scripts/install_wordpress.sh")
 
   tags = {
     Name = "WordPressInstance"
